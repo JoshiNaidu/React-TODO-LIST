40bff11dcd330d5c551361864e8a1e698e483dab
JoshiNaidu <94054346+JoshiNaidu@users.noreply.github.com>
2022-06-09 15:11:23 +0530

    new changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div >
+      <h1>&#169; CopyRights Belongs to ...</h1>
+      {/* both h1 tags give same copyright symbols u can use any of them */}
+      {/* <h1>&copy; CopyRights Belongs to @</h1> */}
+    </div>
   )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import {Navbar,Container,Nav,NavDropdown} from 'react-bootstrap';
 
 function Header() {
   return (
    <>
-   Header
+  <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Container>
+  <Navbar.Brand href="#home">TODO-LIST</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="mx-auto">
+      <Nav.Link href="#features">HOME</Nav.Link>
+      <Nav.Link href="#pricing">ABOUT</Nav.Link>
+      <Nav.Link href="#deets">CONTACT US</Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+  </Container>
+</Navbar>
    </>
   )
 }
